9197fd7ac0997f7397eea093c02011f830f2c967
Craqus07 <ammar9mansoor@gmail.com>
2025-07-04 20:15:01 +0500

    Add playback.py to ps0

---
+++ ps0/playback.py
@@ -0,0 +1,8 @@
+# Taking input
+print("Anything you enter will be slowed down")
+words = input()
+#Replacing spaces with the ...
+slowpb = words.replace(" ","...",-1)
+#outputting the result
+print("Your slowed speech is:")
+print(slowpb)
